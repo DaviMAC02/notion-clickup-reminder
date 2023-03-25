d8534151d8ee3db0d20e92a83a417bd2b391d7f1
Davi Cintra <davicintra@Davis-MacBook-Air.local>
2023-03-25 17:31:24 -0300

    Implemented the first version of the software

---
+++ message_handler.py
@@ -0,0 +1,46 @@
+import json
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+
+# Initialize the Telegram bot using your bot token
+bot = telegram.Bot(token='6297703034:AAHpAtl7LxrNZyBWHREoxVbTNkYyRdkXT7A')
+
+# Set the chat ID that the bot will listen to
+chat_id = '-940741368'
+
+affirmative_responses = ['yes', 'y', 'sim', 's', 'ok', 'okay', 'certo', 'claro', 'affirmative', 'affirmativo', 'positivo']
+
+# Define the function to handle user messages
+def handle_message(update, context):
+    message = update.message.text
+    user_id = update.message.from_user.id
+    print(f"Received message '{message}' from user {user_id}")
+    if message.lower() in affirmative_responses:
+        # Load the user_responded JSON file and update the flag
+        with open('user_responded.json', 'r') as f:
+            print("Updating user_responded.json")
+            user_responded = json.load(f)
+            user_responded["user_responded"] = True
+        # Save the updated JSON file
+        with open('user_responded.json', 'w') as f:
+            json.dump(user_responded, f)
+        # Send a message to the user
+        bot.send_message(chat_id=chat_id, text="Perfeito, bom trabalho e bom descanso")
+
+# Set up the Telegram updater and dispatcher
+updater = Updater(token='6297703034:AAHpAtl7LxrNZyBWHREoxVbTNkYyRdkXT7A', use_context=True)
+dispatcher = updater.dispatcher
+
+# Set up the message handler to respond to user messages
+message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+dispatcher.add_handler(message_handler)
+
+print("Starting bot...")
+
+# Start the Telegram bot and handle CTRL+C
+try:
+    updater.start_polling()
+    print("Bot started")
+    updater.idle()
+except KeyboardInterrupt:
+    updater.stop()
---
+++ script.py
@@ -0,0 +1,64 @@
+import json
+import telegram
+import time
+from datetime import datetime
+
+
+# Initialize the Telegram bot using your bot token
+bot = telegram.Bot(token='6297703034:AAHpAtl7LxrNZyBWHREoxVbTNkYyRdkXT7A')
+
+# Set the chat ID that the bot will send messages to
+chat_id = '-940741368'
+
+# Define user name
+user_name = "Davi"
+
+# Define the message to be sent
+message = f"Olá {user_name}, passando para te lembrar de preencher o seu Notion!" 
+
+# Define the start and end times of the reminder period
+start_time = datetime.strptime('17:00', '%H:%M').time()
+end_time = datetime.strptime('20:00', '%H:%M').time()
+
+# Define the days of the week on which to send the reminder
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+# Define the interval between reminders in seconds
+reminder_interval = 3600 # 1 hour
+
+
+while True:
+    print(f'Today is {datetime.now().strftime("%A")} and the time is {datetime.now().strftime("%H:%M")}, checking if it is time to send a reminder...')
+    now = datetime.now()
+    # Check if it is a weekday and within the reminder period
+    if now.strftime('%A') in days_of_week and start_time <= now.time() <= end_time:
+        print("Reminder period started")
+        # Retrieves the user_responded flag from the JSON file
+        with open('user_responded.json', 'r') as f:
+            user_responded = (json.load(f))['user_responded']
+                
+        # If the user has not responded, send reminders every hour
+        if user_responded == False:
+            while start_time <= datetime.now().time() <= end_time and user_responded == False:
+                bot.send_message(chat_id=chat_id, text=message)
+                now = datetime.now()
+                if now.time() >= end_time:
+                    break
+                time.sleep(reminder_interval)
+                # Retrieves the user_responded flag from the JSON file
+                with open('user_responded.json', 'r') as f:
+                    user_responded = (json.load(f))['user_responded']
+        
+        # Reset the flag for the next day
+        if user_responded == True:
+            with open('user_responded.json', 'r') as f:
+                user_responded = json.load(f)
+                user_responded["user_responded"] = False
+            # Save the updated JSON file
+            with open('user_responded.json', 'w') as f:
+                json.dump(user_responded, f)
+      # Sleep the exact time remaining until the next day
+        print("Sleeping until tomorrow")
+        time.sleep((datetime.combine(now.date(), end_time) - now).seconds)
+                
+      
